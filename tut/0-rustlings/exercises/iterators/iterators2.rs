f06bf15c480fd73299d10ed4bd1211b7f7eb78b4
Jakob Johnson <johnsonjakob99@gmail.com>
2020-01-15 15:27:19 +0000

    iterators done

--- tut/0-rustlings/exercises/iterators/iterators2.rs
+++ tut/0-rustlings/exercises/iterators/iterators2.rs
@@ -5,13 +5,13 @@
 // Step 3. Apply the `capitalize_first` function again to a list, but try and ensure it returns a single string
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.???().collect::<String>() + c.as_str(),
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -35,14 +35,15 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let capitalized_words: Vec<String> = words.iter().map(|x| capitalize_first(x)).collect();
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let capitalized_words = words.iter().map(|x| capitalize_first(x)).collect::<String>();
+        // let capitalized_words = words.iter().map(|x| capitalize_first(x).chars()).flatten().collect::<String>(); //from flatten docs
         assert_eq!(capitalized_words, "Hello World");
     }
 }
--- tut/0-rustlings/exercises/iterators/iterators4.rs
+++ tut/0-rustlings/exercises/iterators/iterators4.rs
@@ -1,6 +1,6 @@
 // iterators4.rs
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return factorial of num
@@ -12,6 +12,9 @@ pub fn factorial(num: u64) -> u64 {
     // For the most fun don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    (1..num+1).product()
+
 }
 
 #[cfg(test)]
