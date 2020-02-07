c9e3e8628389a9b5d498e86601634d50dc15b639
Jakob Johnson <johnsonjakob99@gmail.com>
2020-02-07 17:41:39 +0000

    timer done

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -16,10 +16,29 @@ pub mod shell;
 
 use console::kprintln;
 
+const GPIO_BASE: usize = 0x3F000000 + 0x200000;
+
+const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
+const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
+const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
+
 // FIXME: You need to add dependencies here to
 // test your drivers (Phase 2). Add them as needed.
+use pi::timer::spin_sleep;
+use core::time::Duration;
 
-fn kmain() -> ! {
+unsafe fn kmain() -> ! {
     // FIXME: Start the shell.
-    unimplemented!()
+    // GPIO_FSEL1.write_volatile(1 << 18); //this technically should be a mask and not shifting
+    // GPIO_FSEL1.write_volatile(GPIO_FSEL1.read_volatile() & !(0b111 << 18));
+    GPIO_FSEL1.write_volatile(GPIO_FSEL1.read_volatile() & ! (0b111 << 18));
+//     GPIO_FSEL1.write_volatile(GPIO_FSEL1.read_volatile() | (0b001 << 18));
+    GPIO_FSEL1.write_volatile(GPIO_FSEL1.read_volatile() | (0b001 << 18));
+    loop {
+    GPIO_SET0.write_volatile(1 << 16);
+    spin_sleep(Duration::from_micros(100000));
+    GPIO_CLR0.write_volatile(1 << 16);
+    spin_sleep(Duration::from_micros(100000));
+    
+    }
 }
--- lib/pi/src/timer.rs
+++ lib/pi/src/timer.rs
@@ -5,7 +5,7 @@ use volatile::prelude::*;
 use volatile::{Volatile, ReadVolatile};
 
 /// The base address for the ARM system timer registers.
-const TIMER_REG_BASE: usize = IO_BASE + 0x3000;
+const TIMER_REG_BASE: usize = IO_BASE + 0x3000; //doc says  0x7E003000 but this comes out to be 0x3F003000
 
 #[repr(C)]
 #[allow(non_snake_case)]
@@ -32,17 +32,24 @@ impl Timer {
     /// Reads the system timer's counter and returns Duration.
     /// `CLO` and `CHI` together can represent the number of elapsed microseconds.
     pub fn read(&self) -> Duration {
-        unimplemented!()
+        let clo = self.registers.CLO.read() as u64;
+        let chi = self.registers.CHI.read() as u64;
+        Duration::from_micros(chi << 32 + clo)
     }
 }
 
 /// Returns current time.
 pub fn current_time() -> Duration {
-    unimplemented!()
+    let timer = Timer::new();
+    timer.read()
 }
 
 /// Spins until `t` duration have passed.
 pub fn spin_sleep(t: Duration) {
-    unimplemented!()
+    let target_time = current_time() + t;
+    while current_time() < target_time {
+        //why dont you need a noop here?
+        ;   
+    }
 }
 
